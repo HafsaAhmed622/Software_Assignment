ef5c814f39fd168a8e95e9e8510a6c55784e7d1d
Radwa Mohammed <radwa186@gmail.com>
2026-04-28 18:13:50 +0300

    add delete action

--- finance_tracker/transactions/views.py
+++ finance_tracker/transactions/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import income, expenses
+
 
 # test the new files directory .
 def income_view(request):
@@ -30,3 +31,14 @@ def expense_view(request):
         
     all_expenses = expenses.objects.all().order_by('-Date')
     return render(request, 'track-expenses.html', {'expenses': all_expenses})
+
+
+def delete_income(request, pk):
+    item = get_object_or_404(income, id=pk)
+    item.delete()
+    return redirect('track-income')
+
+def delete_expense(request, pk):
+    item = get_object_or_404(expenses, id=pk)
+    item.delete()
+    return redirect('track-expenses')
